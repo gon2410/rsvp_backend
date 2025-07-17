25a2b98aca5fb44bd99b2fdc27e18231fbf4927d
gon2410 <goonolivera@hotmail.com>
2025-07-17 11:56:39 -0300

    added nextjs host

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://form-supa-next.vercel.app"],
+    allow_origins=["https://form-supa-next.vercel.app", "http://127.0.0.1:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
