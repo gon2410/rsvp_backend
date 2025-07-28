de8fff2e3cc46152151a08a9c160c46657e86c51
gon2410 <goonolivera@hotmail.com>
2025-07-28 10:30:46 -0300

    a lot of changes

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from fastapi.responses import JSONResponse
 from supabase import create_client, Client
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
 import string
+from schemas import *
 
 load_dotenv()
-
 
 url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_KEY")
@@ -20,38 +19,36 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://form-supa-next.vercel.app", "http://127.0.0.1:3000"],
+    allow_origins=["https://form-supa-next.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
 
-@app.get("/auth/status")
-def get_user(request: Request):
-    cookie_name = "sb-" + os.environ.get("SUPABASE_URL").split("https://")[1].split(".")[0] + "-auth-token"
+# ---------- AUTHENTICATION ENDPOINTS ----------
 
-    raw_cookie_header = request.headers.get("cookie", "")
-    token_cookie = None
-    for cookie in raw_cookie_header.split(";"):
-        if cookie_name in cookie:
-            token_cookie = cookie.split("=")[1].strip()
-            break
+# @app.get("/auth/status")
+# def get_user(request: Request):
+#     cookie_name = "sb-" + os.environ.get("SUPABASE_URL").split("https://")[1].split(".")[0] + "-auth-token"
 
-    if not token_cookie:
-        raise HTTPException(status_code=401, detail="No autenticado")
+#     raw_cookie_header = request.headers.get("cookie", "")
+#     token_cookie = None
+#     for cookie in raw_cookie_header.split(";"):
+#         if cookie_name in cookie:
+#             token_cookie = cookie.split("=")[1].strip()
+#             break
+
+#     if not token_cookie:
+#         raise HTTPException(status_code=401, detail="No autenticado")
     
-    try:
-        user_response = supabase.auth.get_user(token_cookie)
-        if user_response.user is None:
-            raise HTTPException(status_code=401, detail="Token inválido")
-        return {"user": user_response.user}
-    except Exception as e:
-        return JSONResponse(status_code=500, content={"error": str(e)})
+#     try:
+#         user_response = supabase.auth.get_user(token_cookie)
+#         if user_response.user is None:
+#             raise HTTPException(status_code=401, detail="Token inválido")
+#         return {"user": user_response.user}
+#     except Exception as e:
+#         return JSONResponse(status_code=500, content={"error": str(e)})
 
-
-class User(BaseModel):
-    email: str
-    passwd: str
 
 @app.post("/auth/login")
 def login_user(user: User, response: Response):
@@ -98,41 +95,33 @@ def logout_user(response: Response):
 
     return response
 
+# ---------- PUBLIC ENDPOINTS ----------
 
 @app.get("/")
 def get_all_leaders():
     try:
-        response = supabase.table("person").select('id', 'name', 'lastname', 'created_at').eq("is_leader", True).order("lastname").execute()
+        response = supabase.table("person").select("id", "name", "lastname", "created_at").eq("is_leader", True).order("lastname").execute()
 
         if response.data.count == 0:
-            return JSONResponse(status_code=404, content={"error": "No se encontró nada en la base de datos"})
+            raise HTTPException(status_code=404, detail="No se encontró nada en la base de datos")
         
         return response.data
     except:
-        return JSONResponse(status_code=500, content={"error": "Hubo un error en la consulta"})
+        raise HTTPException(status_code=500, detail="Algo salió mal de nuestro lado.")
 
 
 @app.get("/get-all-guests")
-def get_all_guests(request: Request):
+def get_all_guests():
     try:
-        response = supabase.table("person").select('*').order("lastname").execute()
+        response = supabase.table("person").select("id", "name", "lastname", "menu", "is_leader", "companion_of").order("lastname").execute()
 
         if response.data.count == 0:
-            return JSONResponse(status_code=404, content={"error": "No se encontró nada en la base de datos"})
+            raise HTTPException(status_code=404, detail="No se encontró nada en la base de datos")
             
         return response.data
     except:
-        return JSONResponse(status_code=500, content={"error": "Hubo un error en la consulta"})
+        return HTTPException(status_code=500, detail="Algo salió mal de nuestro lado.")
 
-
-
-class Guest(BaseModel):
-    name: str
-    lastname: str
-    menu: str
-    role: str
-    email: str
-    leader: str
 
 roles = ["leader", "companion"]
 menus = ["sin_condicion", "vegetariano", "vegano", "celiaco"]
@@ -152,39 +141,38 @@ def add_guest(guest: Guest):
     if guest.role not in roles:
         raise HTTPException(status_code=400, detail="Rol inválido")
     
+    try:
+        response = supabase.table("person").select("id").ilike("name", guest.name).ilike("lastname", guest.lastname).execute()
 
-    response = supabase.table("person").select("id").ilike("name", guest.name).ilike("lastname", guest.lastname).execute()
-
-    if response.data:
-        raise HTTPException(status_code=400, detail="{} {} ya está registrado".format(guest.name, guest.lastname))
-    
-    if guest.role == "leader":
-        if guest.email == "":
-            raise HTTPException(status_code=400, detail="Email inválido")
-        
-        response = supabase.table("person").select("id").eq("email", guest.email).execute()
         if response.data:
-            raise HTTPException(status_code=400, detail="El e-mail ya está registrado. Quizás esta intentando confirmar un acompañante")
-
-        response = supabase.table("person").insert({"name": guest.name, "lastname": guest.lastname, "menu": guest.menu, "email": guest.email, "is_leader": True}).execute()
-        print(response.data)
-    else:
-        if guest.leader == "":
-            raise HTTPException(status_code=400, detail="ID de líder inválido")
-            
-        response = supabase.table("person").select("id").eq("id", guest.leader).execute()
-
-        if not response.data:
-            raise HTTPException(status_code=404, detail="No se encontró un invitado líder con ese ID")
+            raise HTTPException(status_code=400, detail="{} {} ya está registrado".format(guest.name, guest.lastname))
         
-        response = supabase.table("person").insert({"name": guest.name, "lastname": guest.lastname, "menu": guest.menu, "companion_of": guest.leader}).execute()
-        print(response.data)
+        if guest.role == "leader":
+            if guest.email == "":
+                raise HTTPException(status_code=400, detail="Email inválido")
+            
+            response = supabase.table("person").select("id").eq("email", guest.email).execute()
+            if response.data:
+                raise HTTPException(status_code=400, detail="El e-mail ya está registrado. Quizás esta intentando confirmar un acompañante")
 
-    return JSONResponse(status_code=200, content="¡Confirmado!")
+            response = supabase.table("person").insert({"name": guest.name, "lastname": guest.lastname, "menu": guest.menu, "email": guest.email, "is_leader": True}).execute()
+            print(response.data)
+        else:
+            if guest.leader == "":
+                raise HTTPException(status_code=400, detail="ID de líder inválido")
+                
+            response = supabase.table("person").select("id").eq("id", guest.leader).execute()
 
+            if not response.data:
+                raise HTTPException(status_code=404, detail="No se encontró un invitado líder con ese ID")
+            
+            response = supabase.table("person").insert({"name": guest.name, "lastname": guest.lastname, "menu": guest.menu, "companion_of": guest.leader}).execute()
+            print(response.data)
 
-class Group(BaseModel):
-    email: str
+        return JSONResponse(status_code=200, content="¡Confirmado!")
+    except:
+        raise HTTPException(status_code=500, detail="Algo salió mal de nuestro lado.")
+
 
 @app.post("/get-group")
 def get_group(group: Group):
@@ -192,48 +180,68 @@ def get_group(group: Group):
     if group.email == "":
         raise HTTPException(status_code=400, detail="Email inválido")
     
-    response = supabase.table("person").select("*").eq("email", group.email).execute()
+    try:
+        response = supabase.table("person").select("*").eq("email", group.email).execute()
 
-    if not response.data:
-        raise HTTPException(status_code=404, detail="No pudimos encontrar esa direccion de email")
+        if not response.data:
+            raise HTTPException(status_code=404, detail="No pudimos encontrar esa direccion de email.")
 
-    group_list = [response.data[0]]
-    leader_id = response.data[0]["id"]
+        group_list = [response.data[0]]
+        leader_id = response.data[0]["id"]
 
-    response = supabase.table("person").select("*").eq("companion_of", leader_id).execute()
+        response = supabase.table("person").select("*").eq("companion_of", leader_id).execute()
 
-    for member in response.data:
-        group_list.append(member)
+        for member in response.data:
+            group_list.append(member)
 
-    return JSONResponse(status_code=200, content=group_list)
+        return JSONResponse(status_code=200, content=group_list)
+    except:
+        raise HTTPException(status_code=500, detail="Algo salió mal de nuestro lado.")
 
-class Error(BaseModel):
-    email: str
-    description: str
 
 @app.post("/report-error")
 def report_error(error: Error):
 
     if error.email == "":
-        raise HTTPException(status_code=400, detail="Email inválido")
+        raise HTTPException(status_code=400, detail="Email inválido.")
 
-    response = supabase.table("person").select("*").eq("email", error.email).execute()
-    if not response.data:
-        raise HTTPException(status_code=404, detail="No pudimos encontrar esa direccion de email")
-    
     try:
-        response = supabase.table("errors").insert({"email": error.email, "description": error.description}).execute()
-    except Exception as e:
-        print(e)
-        raise HTTPException(status_code=400, detail="Algo salió mal.")
+        response = supabase.table("person").select("*").eq("email", error.email).execute()
+        if not response.data:
+            raise HTTPException(status_code=404, detail="No pudimos encontrar esa direccion de email.")
+        
+        try:
+            response = supabase.table("errors").insert({"email": error.email, "description": error.description}).execute()
+        except Exception as e:
+            print(e)
+            raise HTTPException(status_code=400, detail="Algo salió mal.")
 
-    return JSONResponse(status_code=200, content="Enviado")
+        return JSONResponse(status_code=200, content="Enviado.")
+    except:
+        raise HTTPException(status_code=500, detail="Algo salió mal de nuestro lado.")
 
-class EditGuest(BaseModel):
-    id: str
-    name: str
-    lastname: str
-    menu: str
+
+@app.get("/get-statistics")
+def get_numbers():
+    try:
+        total = supabase.table("person").select("id", count="exact").execute()
+        sin_condicion = supabase.table("person").select("id", count="exact").eq("menu", "sin_condicion").execute()
+        vegetariano = supabase.table("person").select("id", count="exact").eq("menu", "vegetariano").execute()
+        vegano = supabase.table("person").select("id", count="exact").eq("menu", "vegano").execute()
+        celiaco = supabase.table("person").select("id", count="exact").eq("menu", "celiaco").execute()
+        
+        data = [{"menu_name": "Total", "quantity": total.count},
+                {"menu_name": "Sin Condicion", "quantity": sin_condicion.count},
+                {"menu_name": "Vegetariano", "quantity": vegetariano.count},
+                {"menu_name": "Vegano", "quantity": vegano.count},
+                {"menu_name": "Celiaco", "quantity": celiaco.count}]
+        
+        return JSONResponse(status_code=200, content=data)
+    except:
+        raise HTTPException(status_code=500, detail="Algo salió mal de nuestro lado.")
+
+
+# ---------- PROTECTED ENDPOINTS ----------
 
 @app.post("/update-guest")
 def edit_guest(edit_guest: EditGuest, request: Request):
@@ -244,23 +252,22 @@ def edit_guest(edit_guest: EditGuest, request: Request):
             break
 
     if edit_guest.name == "" or any(invalid_character in edit_guest.name for invalid_character in invalid_characters):
-        raise HTTPException(status_code=400, detail="Nombre inválido")
+        raise HTTPException(status_code=400, detail="Nombre inválido.")
         
     if edit_guest.lastname == "" or any(invalid_character in edit_guest.lastname for invalid_character in invalid_characters):
-        raise HTTPException(status_code=400, detail="Apellido inválido")
+        raise HTTPException(status_code=400, detail="Apellido inválido.")
     
     if not cookie_token:
-        raise HTTPException(status_code=401, detail="Prohibido si no estas logueado")
+        raise HTTPException(status_code=401, detail="Prohibido si no estas logueado.")
     try:
         response = supabase.table("person").update({"name": edit_guest.name, "lastname": edit_guest.lastname, "menu": edit_guest.menu}).eq("id", edit_guest.id).execute()
     except Exception as e:
         print(e)
         raise HTTPException(status_code=400, detail="Algo salio mal.")
 
-    return JSONResponse(status_code=200, content="Guardado")
+    return JSONResponse(status_code=200, content="Guardado.")
 
-class DeleteGuest(BaseModel):
-    id: int
+
 @app.post("/delete-guest")
 def delete_guest(guest_to_delete: DeleteGuest, request: Request):
     cookie_token = None
@@ -276,35 +283,18 @@ def delete_guest(guest_to_delete: DeleteGuest, request: Request):
         response = supabase.table("person").select("id", "is_leader").eq("id", guest_to_delete.id).execute()
         
         if not response:
-            raise HTTPException(status_code=404, detail="No pudimos encontrar al invitado")
+            raise HTTPException(status_code=404, detail="No pudimos encontrar al invitado.")
         
         guest = response.data[0]
         
         if guest["is_leader"] == True:
-            raise HTTPException(status_code=400, detail="No se puede eliminar a líderes")
+            raise HTTPException(status_code=400, detail="No se puede eliminar a líderes.")
         else:
             try:
                 response = supabase.table("person").delete().eq("id", guest_to_delete.id).execute()
             except Exception as e:
-                raise HTTPException(status_code=400, detail="No se pudo eliminar al invitado")
-    except Exception as e:
-        print(e)
-        
-
-    return JSONResponse(status_code=200, content="Eliminado")
-
-@app.get("/get-statistics")
-def get_numbers():
-    total = supabase.table("person").select("id", count="exact").execute()
-    sin_condicion = supabase.table("person").select("id", count="exact").eq("menu", "sin_condicion").execute()
-    vegetariano = supabase.table("person").select("id", count="exact").eq("menu", "vegetariano").execute()
-    vegano = supabase.table("person").select("id", count="exact").eq("menu", "vegano").execute()
-    celiaco = supabase.table("person").select("id", count="exact").eq("menu", "celiaco").execute()
-    
-    data = [{"menu_name": "Total", "quantity": total.count},
-            {"menu_name": "Sin Condicion", "quantity": sin_condicion.count},
-            {"menu_name": "Vegetariano", "quantity": vegetariano.count},
-            {"menu_name": "Vegano", "quantity": vegano.count},
-            {"menu_name": "Celiaco", "quantity": celiaco.count}]
-    
-    return JSONResponse(status_code=200, content=data)
+                raise HTTPException(status_code=400, detail="No se pudo eliminar al invitado.")
+            
+        return JSONResponse(status_code=200, content="Eliminado.")  
+    except:
+        raise HTTPException(status_code=500, detail="Algo salió mal de nuestro lado.")
